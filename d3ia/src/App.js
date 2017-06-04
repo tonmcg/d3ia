89fe8aeaac30a9d27da7e437fc7061f59e08dbff
tonmcg <tonmcg+c9@gmail.com>
2017-06-04 17:09:25 +0000

    branch update

---
+++ d3ia/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import WorldMap from './WorldMap';
+import BarChart from './BarChart';
+import worlddata from './world';
+import { range } from 'd3-array';
+import { scaleThreshold } from 'd3-scale';
+import { geoCentroid } from 'd3-geo';
+
+const appdata = worlddata.features.filter(d => geoCentroid(d)[0] < -20);
+
+appdata.forEach((d, i) => {
+    const offset = Math.random();
+    d.launchday = i;
+    d.data = range(30).map((p, q) => q < i ? 0 : Math.random() * 2 + offset);
+});
+
+const colorScale = scaleThreshold().domain([5, 10, 20, 30, 50]).range(["#75739F", "#5EAFC6", "#41A368", "#93C464", "#FE9922"]);
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>d3ia dashboard</h2>
+                </div>
+                <div>
+                    <WorldMap colorScale={colorScale} data={appdata} size={[500,400]} />
+                    <BarChart colorScale={colorScale} data={appdata} size={[500,400]} />
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
